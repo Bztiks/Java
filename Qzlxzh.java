3c42cbc8aa5b880ac520731d0790ab40236a7c0e
北褶tiks <2777973423@qq.com>
2021-09-11 09:52:45 +0800

    day5

--- Qzlxzh.java
+++ Qzlxzh.java
@@ -2,7 +2,6 @@ public class Qzlxzh{
     public static void main(String[] args) {
         double x = 9.8127381367;
         int nx = (int) x;
-        int dx = (int) nx;
         System.out.print(nx);
     }
 }
---
+++ extern.java
@@ -0,0 +1,10 @@
+import jdk.internal.agent.resources.agent_de;
+
+public class extern {
+    public static void main(String[] args) {
+        int a = 1;
+
+        System.out.print(a);
+    }
+    
+}
